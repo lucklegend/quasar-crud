ee06fb36cff47778b486ff35acd5a9833e11e7e3
Janno E. Nuyles <lucklegend@gmail.com>
2023-06-03 16:44:06 +0800

    first commit

---
+++ src/components/modals/LoginModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-card class="q-pa-md shadow-2 my_card" bordered>
+    <q-card-section class="text-center">
+      <div class="text-grey-9 text-h5 text-weight-bold">Sign in</div>
+      <div class="text-grey-8">Sign in below to access your account</div>
+    </q-card-section>
+    <q-card-section>
+      <q-input dense outlined v-model="email" label="Email Address"></q-input>
+      <q-input
+        dense
+        outlined
+        class="q-mt-md"
+        v-model="password"
+        type="password"
+        label="Password"
+      ></q-input>
+    </q-card-section>
+    <q-card-section>
+      <q-btn
+        style="border-radius: 8px"
+        color="dark"
+        rounded
+        size="md"
+        label="Sign in"
+        no-caps
+        class="full-width"
+      ></q-btn>
+    </q-card-section>
+    <q-card-section class="text-center q-pt-none">
+      <div class="text-grey-8">
+        Don't have an account yet?
+        <a
+          href="#"
+          class="text-dark text-weight-bold"
+          style="text-decoration: none"
+          >Sign up.</a
+        >
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+  name: "LoginModal",
+
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+});
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <q-space />
+        <q-btn
+          flat
+          round
+          dense
+          icon="login"
+          class="q-mr-sm"
+          @click="loginForm"
+        />
+        <q-btn flat round dense icon="more_vert" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+
+  data() {
+    return {
+      is_login_modal: false,
+    };
+  },
+
+  methods: {
+    LoginForm() {},
+  },
+});
+</script>
